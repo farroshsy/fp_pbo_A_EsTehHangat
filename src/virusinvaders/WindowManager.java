781736dfd3dfa80f8520a4b869e4e1dcd162f9c8
abhiramadamas <74411226+abhiramadamas@users.noreply.github.com>
2021-12-22 19:34:28 +0700

    Add files via upload

---
+++ src/virusinvaders/WindowManager.java
@@ -0,0 +1,48 @@
+package virusinvaders;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class WindowManager {
+
+	public static final byte ZOOM = 3;
+	
+	public static final int WIDTH = 256 * ZOOM;
+	public static final int HEIGHT = 219 * ZOOM;
+	
+	public static final int LIMIT_LEFT = 15 * ZOOM;
+	public static final int LIMIT_RIGHT = WIDTH - 15 * ZOOM;
+	
+	public static final int LIMIT_UP = 30 * ZOOM;
+	public static final int LIMIT_DOWN = HEIGHT - 20 * ZOOM;
+	
+	private final JFrame window;
+	
+	public WindowManager(Game gamePanel) {
+		//Create the window
+		this.window = new JFrame("Virus Invaders");
+		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.window.setResizable(false);
+		
+		//Set the game panel's size
+		gamePanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		//Add key listener to panel
+		gamePanel.setFocusable(true);
+		gamePanel.addKeyListener(gamePanel);
+		
+		//Add game panel to window
+		this.window.setContentPane(gamePanel);
+		
+		//Needed for key listener
+		gamePanel.requestFocusInWindow();
+		
+		//Set the window size as the size of its content
+		this.window.pack();
+	}
+	
+	public void setVisible() {
+		this.window.setVisible(true);
+	}
+}
