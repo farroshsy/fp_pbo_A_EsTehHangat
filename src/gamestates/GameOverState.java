87c42cbcc326249f41a7fdf0173cc025f56c2c80
abhiramadamas <74411226+abhiramadamas@users.noreply.github.com>
2021-12-22 19:37:25 +0700

    Add files via upload

---
+++ src/gamestates/GameOverState.java
@@ -0,0 +1,46 @@
+package gamestates;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import utils.Audio;
+import utils.Backgrounds;
+import utils.Resources;
+
+public class GameOverState extends GameState {
+	
+	private int score;
+
+	public GameOverState(GameStateManager gameStateManager, int score) {
+		super(gameStateManager);
+		//When game over state is created the new score is registered and saved to the file
+		Resources.registerNewScore(score);
+		Resources.writeHighScores();
+		this.score = score;
+		Audio.playSound("/sound/Ambulance.wav");
+	}
+
+	@Override
+	public void init() {}
+
+	@Override
+	public void loop() {}
+
+	@Override
+	public void draw(Graphics graphics) {
+		//Background drawing moved in another class to be more organized
+		Backgrounds.drawGameOverScreen(graphics, score);
+	}
+
+	@Override
+	public void keyPressed(int keyCode) {
+		//Clear game state stack and go to main menu
+		if(keyCode == KeyEvent.VK_ESCAPE) {
+			super.gameStateManager.clearStack(new MainMenu(gameStateManager));
+		}
+	}
+
+	@Override
+	public void keyReleased(int keyCode) {}
+
+}
